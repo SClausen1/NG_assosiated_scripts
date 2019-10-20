edca3111e1821ee95ea02274741af85db72e8799
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2019-10-20 17:47:37 -0400

    Updated export.py
    Sped up export, added ESCAPE to exit, and fixed event=v filename bug

--- export_data.py
+++ export_data.py
@@ -8,34 +8,53 @@ import json
 import sys
 import re
 
-def main(argv):
-    
+from pynput.keyboard import Key, Listener #for key press
+import threading
+
+global stop_export
+stop_export = False
+
+# Main export function
+def main():
+    global stop_export
+
+    if not len(sys.argv) == 2:
+        print("File name expected as command line argument")
+        exit()
+
+    filename = sys.argv[1]
+
     # expects mp4 files to be in the static directory 
     # and of the form 'participant_number'_zed_trimmed.mp4
-    json_file_name = 'static\\' + argv.replace('.mp4','_dataset.json')
-    video_path = 'static\\' + argv
+    json_file_name = filename.replace('.mp4','_dataset.json')
+    video_path = filename
     if os.path.exists(json_file_name):
         json_file = open(json_file_name, "r")
         label_data = json.load(json_file)
-        p_num = re.match("\d+", argv)
-        export_path = 'export\p' + p_num[0]
+        p_num = filename[filename.rfind('/')+1:].split('_')[0]
+        export_path = 'export/p' + p_num
 
         if not os.path.exists(export_path):
             os.makedirs(export_path)
         
         if os.path.exists(video_path):
             for label_key in label_data:
-                
-                label_type = label_data[label_key][0]
-                time_in = str(float(label_data[label_key][1]) / 1000) # convert ms to second
-                time_out = str(float(label_data[label_key][2]) / 1000)
-                label = label_data[label_key][3].replace(" ", "_" )
-                label_type_abr = label_type[0]
 
-                if label_type == 'Event':
+                if stop_export:
+                    exit()
+                
+                time_in = float(label_data[label_key][1]) / 1000 # convert ms to second
+                time_out = float(label_data[label_key][2]) / 1000 - time_in
+                label = label_data[label_key][3].replace(" ", "_" )
+                label_type = label_data[label_key][0]
+                label_type_abr = label_type[0]
+                if label_type.lower() == 'event': #e=emotion, a=action, s=speech, v=event
                     label_type_abr = 'v'
 
                 for ind in range(1,7): #Cameras
+
+                    if stop_export:
+                        exit()
                     
                     # used for splitting the camera frames
                     row = int((ind - 1) / 2)
@@ -43,16 +62,25 @@ def main(argv):
                     x = 1280 * (col + 1)
                     y = 720 * row
                 
-                    output_vid = 'export\p' + p_num[0] + '\\' + label_type_abr + '_' + label +'_cam' + str(ind) + '_'  + str(time_in) + '.mp4'
+                    output_vid = export_path + '/' + label_type_abr + '_' + label +'_cam' + str(ind) + '_'  + str(time_in) + '.mp4'
                     
                     if not os.path.isfile(output_vid):
-                        cmd = 'ffmpeg -i ' + video_path + ' -ss ' + time_in + ' -strict -2 -to ' + time_out +  ' -filter:v "crop=1280:720:' + str(x) + ':' + str(y) + '" ' + output_vid
+                        cmd = 'ffmpeg -ss ' + str(time_in) + ' -i ' + video_path + ' -strict -2 -t ' + str(time_out) +  ' -filter:v "crop=1280:720:' + str(x) + ':' + str(y) + '" ' + output_vid
                         os.system(cmd)
 
-
+# Key press listener
+def on_press(key):
+    global stop_export
+    if key == Key.esc:
+        stop_export = True
+        exit()
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        print("File name expected as command line argument")
+
+    # Use thread so we can press ESCAPE key to cancel
+    t = threading.Thread(target=main)
+    t.start()
+    
+    # Keybooard key listener
+    with Listener(on_press=on_press) as listener: listener.join()
+
